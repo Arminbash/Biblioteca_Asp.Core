1593f9a37b14f552abda44a8d5516e3ab5f77d3a
Johnny Arcia <johnnyarcia96@gmail.com>
2019-09-26 17:25:10 -0600

    Migracion buena

---
+++ Biblioteca_AspCore3/Biblioteca.Persistence.MSSQL/Migrations/20190926232358_InitialCreate.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Biblioteca.Persistence.MSSQL.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autor",
+                columns: table => new
+                {
+                    IdAutor = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Nacionalidad = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autor", x => x.IdAutor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiante",
+                columns: table => new
+                {
+                    IdEstudiante = table.Column<int>(type: "Int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CI = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Nombre = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Direccion = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Carrera = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Edad = table.Column<int>(type: "Int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiante", x => x.IdEstudiante);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Libro",
+                columns: table => new
+                {
+                    IdLibro = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Titulo = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Editorial = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Area = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libro", x => x.IdLibro);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LibAut",
+                columns: table => new
+                {
+                    IdLibAut = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AutorIdAutor = table.Column<int>(nullable: true),
+                    LibroIdLibro = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LibAut", x => x.IdLibAut);
+                    table.ForeignKey(
+                        name: "FK_LibAut_Autor_AutorIdAutor",
+                        column: x => x.AutorIdAutor,
+                        principalTable: "Autor",
+                        principalColumn: "IdAutor",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_LibAut_Libro_LibroIdLibro",
+                        column: x => x.LibroIdLibro,
+                        principalTable: "Libro",
+                        principalColumn: "IdLibro",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestamo",
+                columns: table => new
+                {
+                    IdPrestamo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FechaPrestamo = table.Column<DateTime>(type: "datetime", nullable: false),
+                    FechaDevolucion = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Devolucion = table.Column<bool>(type: "bit", nullable: false),
+                    LibroIdLibro = table.Column<int>(nullable: true),
+                    EstudianteIdEstudiante = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamo", x => x.IdPrestamo);
+                    table.ForeignKey(
+                        name: "FK_Prestamo_Estudiante_EstudianteIdEstudiante",
+                        column: x => x.EstudianteIdEstudiante,
+                        principalTable: "Estudiante",
+                        principalColumn: "IdEstudiante",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Prestamo_Libro_LibroIdLibro",
+                        column: x => x.LibroIdLibro,
+                        principalTable: "Libro",
+                        principalColumn: "IdLibro",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibAut_AutorIdAutor",
+                table: "LibAut",
+                column: "AutorIdAutor");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LibAut_LibroIdLibro",
+                table: "LibAut",
+                column: "LibroIdLibro");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prestamo_EstudianteIdEstudiante",
+                table: "Prestamo",
+                column: "EstudianteIdEstudiante");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prestamo_LibroIdLibro",
+                table: "Prestamo",
+                column: "LibroIdLibro");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LibAut");
+
+            migrationBuilder.DropTable(
+                name: "Prestamo");
+
+            migrationBuilder.DropTable(
+                name: "Autor");
+
+            migrationBuilder.DropTable(
+                name: "Estudiante");
+
+            migrationBuilder.DropTable(
+                name: "Libro");
+        }
+    }
+}
